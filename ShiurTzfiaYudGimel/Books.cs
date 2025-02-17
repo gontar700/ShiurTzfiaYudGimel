56be0973c22852ae93e563e2e39843e39b8ce43b
danielgontar@gmail.com <danielgontar@gmail.com>
2025-02-17 11:40:02 +0200

    Add project files.

---
+++ ShiurTzfiaYudGimel/Books.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects
+{
+    class Book
+    {
+        // הגדרת תכונות
+        private string name;    // שם הספר
+        private string writer;  // שם הסופר
+        private int pages;      // מספר עמודים
+        private double price;   // מחיר הספר
+
+        // פעולה בונה 
+        public Book(string name, string writer, int pages, double price)
+        {
+            this.name = name;
+            this.writer = writer;
+            this.pages = pages;
+            this.price = price;
+        }
+        // פעולות מאחזרות
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public string GetWriter()
+        {
+            return this.writer;
+        }
+
+        public int GetPages()
+        {
+            return this.pages;
+        }
+
+        public double GetPrice()
+        {
+            return this.price;
+        }
+
+        // פעולות קובעות
+        public void SetPrice(double price)
+        {
+            this.price = price;
+        }
+
+        // פעולות חישוביות
+        public void ChangePrice(double precent)
+        {
+            this.price += this.price * precent / 100;
+        }
+
+        // פעולה לתיאור העצם
+        public override string ToString()
+        {
+            string desc = "";
+            desc = "name:" + this.name + "\n";
+            desc += "writer:" + this.writer + "\n";
+            desc += "pages:" + this.pages + "\n";
+            desc += "price:" + this.price + "\n";
+            return desc;
+        }
+    }
+}
---
+++ ShiurTzfiaYudGimel/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Objects;
+
+
+namespace ShiurTzfiaYudGimel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World");
+            Book book = new Book("Katav Hazit", "Ron Ben Ishai", 455, 89.90);
+            Console.WriteLine(book.GetPrice());
+            book.ChangePrice(20);
+            Console.WriteLine("Price increased in 20%");
+            Console.WriteLine(book.GetPrice());
+        }
+    }
+}
